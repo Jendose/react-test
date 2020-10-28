91f42d74b7b20efcd8eee729d7ae9d4db4e78ce7
Jendose <zhenya.shatskov@yandex.ru>
2020-10-28 21:48:04 +0400

    Add common page parts and styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/Header.css';
+import logo from '../images/logo.png';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="header">
+                <div className="container">
+                    <div className="header__row">
+                        <div className="header__logo"><a href='/'><img src={logo}/></a></div>
+                        <div className="header__menu menu">
+                            <div className="menu__icon icon-menu">
+                                <span></span>
+                            </div>
+                            <div className="menu__list">
+                                <div className="menu__element">Tracks Database</div>
+                                <div className="menu__element">Liked Tracks</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header
---
+++ src/components/Track.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Track.css';
+
+class Track extends React.Component {
+    render() {
+        return (
+            <div>
+                {/* Трек */}
+            </div>
+        )
+    }
+}
